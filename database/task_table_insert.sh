cb62bb053a98d933180288d008da80d4b8df0b3d
Steven Liu <lingjiujianke@gmail.com>
2017-03-09 23:28:19 +0800

    add task pid into database
    insert task info when create a new transcode task

--- database/task_table_insert.sh
+++ database/task_table_insert.sh
@@ -1,67 +1,16 @@
 #!/bin/bash
 
 declare -a SRC_FILENAMES
+SRC_FILE_NAME=$1
+DST_FILE_NAME=$2
+TEMPLATE_NAME=$3
+TASK_PID=$4
 
-################## Read file name for task inputs ################
-FILE_INFO_INSERT_SQL="select dst_filename from cmts_files where status = 0";
-echo ${FILE_INFO_INSERT_SQL}
-PROVERB="`mysql --user='root' --password='root' --database='cmts' --execute=\"$FILE_INFO_INSERT_SQL\" | awk -F"dst_filename" '{ print $1 }'` "
-read -a SRC_FILENAMES <<< $PROVERB
-STREAM_NUM=${#SRC_FILENAMES[@]}
-for((i=0;i<$STREAM_NUM;i++))
-{
-    echo "=== ${SRC_FILENAMES[$i]}"
-}
+################## Insert transcode task into cmts_task #################
 
-
-################## Read template for transdor #################
-TEMPLATE_NAME_SELECT_SQL="select template_name from cmts_template";
-#echo ${TEMPLATE_NAME_SELECT_SQL}
-TEMPLATE_NAME="`mysql --user='root' --password='root' --database='cmts' --execute=\"$TEMPLATE_NAME_SELECT_SQL\" | awk -F"template_name" '{ print $1 }' | grep -o \"[^ ]\+\( \+[^ ]\+\)*\"` "
-if [[ $TEMPLATE_NAME == "NULL" ]]
-then
-    TEMPLATE_NAME=""
-fi
-
-TEMPLATE_FILTER_PARAM_SELECT_SQL="select filter_param from cmts_template";
-#echo ${TEMPLATE_FILTER_PARAM_SELECT_SQL}
-TEMPLATE_FILTER_PARAM_NAME="`mysql --user='root' --password='root' --database='cmts' --execute=\"$TEMPLATE_FILTER_PARAM_SELECT_SQL\" | awk -F"filter_param" '{ print $1 }' | grep -o \"[^ ]\+\( \+[^ ]\+\)*\"`"
-if [[ $TEMPLATE_FILTER_PARAM_NAME == "NULL" ]]
-then
-    TEMPLATE_FILTER_PARAM_NAME=""
-fi
-
-TEMPLATE_VIDEO_PARAM_SELECT_SQL="select video_param from cmts_template";
-#echo "${TEMPLATE_VIDEO_PARAM_SELECT_SQL}"
-TEMPLATE_VIDEO_PARAM_NAME="`mysql --user='root' --password='root' --database='cmts' --execute=\"$TEMPLATE_VIDEO_PARAM_SELECT_SQL\" | awk -F"video_param" '{ print $1 }' | grep -o \"[^ ]\+\( \+[^ ]\+\)*\"` "
-if [[ $TEMPLATE_VIDEO_PARAM_NAME == "NULL" ]]
-then
-    TEMPLATE_VIDEO_PARAM_NAME=""
-fi
-
-TEMPLATE_AUDIO_PARAM_SELECT_SQL="select audio_param from cmts_template";
+TASK_INSERT_SQL="insert into cmts_task (input, template_name, output, task_pid) value (\"$SRC_FILE_NAME\", \"${DST_FILE_NAME}\", \"${TEMPLATE_NAME}\", \"${TASK_PID}\" )"
 #echo ${TEMPLATE_AUDIO_PARAM_SELECT_SQL}
-TEMPLATE_AUDIO_PARAM_NAME="`mysql --user='root' --password='root' --database='cmts' --execute=\"$TEMPLATE_AUDIO_PARAM_SELECT_SQL\" | awk -F"audio_param" '{ print $1 }' | grep -o \"[^ ]\+\( \+[^ ]\+\)*\"` "
-if [[ $TEMPLATE_AUDIO_PARAM_NAME == "NULL" ]]
-then
-    TEMPLATE_AUDIO_PARAM_NAME=""
-fi
-
-########## strcat ffmpeg command line ######
-#echo "[ $TEMPLATE_NAME ]"
-
-#echo "[ ${TEMPLATE_FILTER_PARAM_NAME} ]"
-
-#echo "[ $TEMPLATE_VIDEO_PARAM_NAME ]"
-
-#echo "[ $TEMPLATE_AUDIO_PARAM_NAME ]"
-
-for((i=0;i<$STREAM_NUM;i++))
-{
-    TEMP_OUTPUT_FILE_NAME=${SRC_FILENAMES[$i]};
-    OUTPUT_FILE_NAME="${TEMP_OUTPUT_FILE_NAME%%.*}_$TEMPLATE_NAME.m3u8"
-    echo "ffmpeg -i ${SRC_FILENAMES[$i]}  ${TEMPLATE_FILTER_PARAM_NAME} $TEMPLATE_VIDEO_PARAM_NAME  $TEMPLATE_AUDIO_PARAM_NAME  -hls_list_size 0 $OUTPUT_FILE_NAME"
-}
+mysql --user='root' --password='root' --database='cmts' --execute="$TASK_INSERT_SQL"
 
 
 
--- database/transcode.sh
+++ database/transcode.sh
@@ -16,4 +16,13 @@ CMD_STRING="ffmpeg -i ${INPUT_FILE} ${GET_TRANSCODE_PARAM} ${DST_FILE_NAME}"
 
 echo "${CMD_STRING}"
 
-/usr/local/openresty/nginx/html/scheduler -c "${CMD_STRING}" -a 127.0.0.1
+SCHEDULER_RESULT=`/usr/local/openresty/nginx/html/scheduler -c "${CMD_STRING}" -a 127.0.0.1 | grep "waiting pid=" | awk -F"pid=" '{ print $2 }' | awk -F"]" '{ print $1 }'`
+
+echo "($SCHEDULER_RESULT)"
+
+
+SET_TASK_INSERT=`/usr/local/openresty/nginx/html/task_table_insert.sh "{$INPUT_FILE}" "${DST_FILE_NAME}" "${TEMPLATE_NAME}" "${SCHEDULER_RESULT}"`
+
+
+
+
